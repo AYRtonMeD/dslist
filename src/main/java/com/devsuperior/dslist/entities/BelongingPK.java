5baa684b1e8ffa8a09082944dc30cce82956e586
AYRtonMeD <ayrtondantas2013@gmail.com>
2025-01-22 19:28:02 -0300

    feat: implementa entidades

---
+++ src/main/java/com/devsuperior/dslist/entities/BelongingPK.java
@@ -0,0 +1,41 @@
+package com.devsuperior.dslist.entities;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Embeddable
+public class BelongingPK {
+    @ManyToOne
+    @JoinColumn(name = "game_id")
+    private Game game;
+
+    @ManyToOne
+    @JoinColumn(name = "list_id")
+    private GameList gameList;
+
+    public BelongingPK(Game game, GameList gameList) {
+        this.game = game;
+        this.gameList = gameList;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        BelongingPK that = (BelongingPK) o;
+        return Objects.equals(game, that.game) && Objects.equals(gameList, that.gameList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(game, gameList);
+    }
+}
